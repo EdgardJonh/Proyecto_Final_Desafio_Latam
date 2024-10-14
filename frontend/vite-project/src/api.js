f2db3bf8143fd67c0a7bd434ea2734a95be6086e
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-10-14 00:48:59 -0300

    se realizan cambios en el api.js frontend

--- frontend/vite-project/src/api.js
+++ frontend/vite-project/src/api.js
@@ -28,20 +28,7 @@ export const login = async (email, password) => {
 export const register = (name, email, password) => api.post('/users/register', { name, email, password });
 export const getProfile = () => api.get('/users/profile');
 
-export const getAllProducts = async () => {
-  try {
-    const response = await api.get('/products');
-    // Asegúrate de que todos los precios sean números
-    return response.data.map(product => ({
-      ...product,
-      price: parseFloat(product.price)
-    }));
-  } catch (error) {
-    console.error('Error al obtener productos:', error);
-    throw error;
-  }
-};
-
+export const getAllProducts = () => api.get('/products');
 export const getProduct = (id) => api.get(`/products/${id}`);
 export const createProduct = (productData) => api.post('/products', productData);
 export const updateProduct = (id, productData) => api.put(`/products/${id}`, productData);
